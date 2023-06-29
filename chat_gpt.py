ee25477c0969f9419a92e6496b11d0711a8c5c24
Zihao Wang <zihaowangzihao@gmail.com>
2023-06-29 11:51:52 -0700

    Test

--- chat_gpt.py
+++ chat_gpt.py
@@ -1,7 +1,35 @@
-import os
-from dotenv import load_dotenv
+import openai
+import requests
+from api_key import *
 
-def load_api_keys():
-    load_dotenv()
-load_api_keys()
-os.getenv("API_KEY")
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f"Bearer {api_key}"
+}
+
+def gpt(role, question):
+    """
+    :param role: <str> - role gpt takes on
+    :param question: <str> - question you ask gpt
+    :return: <str> - gpt response
+    """
+    data = {
+        'messages': [
+            {'role': 'system', 'content': role},
+            {'role': 'user', 'content': question}
+        ],
+        'model': 'gpt-3.5-turbo'
+    }
+
+    api_url = 'https://api.openai.com/v1/chat/completions'
+    response = requests.post(api_url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        response_data = response.json()
+        reply = response_data['choices'][0]['message']['content']
+        return reply
+    else:
+        message = 'Error:', response.status_code, response.text
+        return message
+
+
